027149a415e7f3b6bb64663554569161dad0577e
Anugrah Prastyo <anugrahprastyo13@gmail.com>
2024-07-04 20:43:51 +0700

    refactor[first_commit_github.html & tailwind.config.js] - changes dan remove config

--- src/assets/style/js/index-c0c31320795846.js
+++ src/assets/style/js/index-c0c31320795846.js
@@ -145,29 +145,18 @@ themeCheck();
 // }
 
 const copyButton = document.getElementById('copy-button');
-const copyButtonSecond = document.getElementById('copy-button-second');
 const codeSnippet = document.getElementById('code-snippet');
-const codeSnippetSecond = document.getElementById('code-snippet-second');
 
 copyButton.addEventListener('click', () => {
     navigator.clipboard.writeText(codeSnippet.innerText).then(() => {
         const originalText = copyButton.innerHTML;
-        copyButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-copy"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" /><path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg>Copied';
+        copyButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-copy"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" /><path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg>Copied';
         setTimeout(() => {
             copyButton.innerHTML = originalText;
         }, 2000);
     });
 });
 
-copyButtonSecond.addEventListener('click', () => {
-    navigator.clipboard.writeText(codeSnippetSecond.innerText).then(() => {
-        const originalText = copyButtonSecond.innerHTML;
-        copyButtonSecond.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-copy"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" /><path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" /></svg>Copied';
-        setTimeout(() => {
-            copyButtonSecond.innerHTML = originalText;
-        }, 2000);
-    });
-});
 //Navbar Fixed
 window.onscroll = function() {
     const header = document.querySelector('header');
